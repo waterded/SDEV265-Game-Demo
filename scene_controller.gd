9b36900cb7ef8eabd4ded8f508af4ed3f6f9aec5
waterded <120753644+waterded@users.noreply.github.com>
2026-02-24 19:48:44 -0500

    Added Credits

--- scene_controller.gd
+++ scene_controller.gd
@@ -14,3 +14,7 @@ func change_scene(scene_path: String):
     print("loading ",scene_path)
     var new_scene = load(scene_path).instantiate()
     scene_container.add_child(new_scene)
+
+func _unhandled_key_input(event: InputEvent) -> void:
+    if event.is_action_pressed("debug_hammer"):
+        GameData.player_items[2]=load("res://Combat System/Items/Player Items/debug_hammer.tres")
